1897d607b26c00cbe561b5d687f60db6c9971002
Adrian Almeida <adrianalmeida417@gmail.com>
2025-07-25 17:24:31 -0300

    chore: adding name in Pooler

--- src/worker/src/main/kotlin/com/example/worker/core/pool/Pooler.kt
+++ src/worker/src/main/kotlin/com/example/worker/core/pool/Pooler.kt
@@ -4,21 +4,33 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.slf4j.LoggerFactory
 
 class Pooler(
     private val scope: CoroutineScope
 ) {
 
-    private var processor: Processor? = null
-    private var size: Int = 1
+    private val log = LoggerFactory.getLogger(Pooler::class.java)
 
-    fun of(processor: Processor) : Pooler {
-        this.processor = processor
+    private var _name: String? = null
+    private var size: Int = 1
+    private var processor: Processor? = null
+
+    val name: String
+        get() = _name ?: "Pooler@$processor"
+
+    fun name(name: String?) : Pooler {
+        this._name = name
         return this
     }
 
-    fun size(size: Int) : Pooler {
-        this.size = size
+    fun size(size: Int?) : Pooler {
+        this.size = size ?: this.size
+        return this
+    }
+
+    fun of(processor: Processor) : Pooler {
+        this.processor = processor
         return this
     }
 
@@ -28,6 +40,7 @@ class Pooler(
         if (size <= 0)
             throw RuntimeException("Pooler size must be greater than 0")
 
+        log.info("Starting processor: ${name ?: processor}...")
         repeat(size) {
             scope.launch {
                 withContext(Dispatchers.IO) {
