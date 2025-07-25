e21469d572a3ead0646870f014c12aa619a1f241
Adrian Almeida <adrianalmeida417@gmail.com>
2025-07-25 19:17:58 -0300

    fix: improving Dependencies instances

--- src/worker/src/main/kotlin/com/example/worker/core/Dependencies.kt
+++ src/worker/src/main/kotlin/com/example/worker/core/Dependencies.kt
@@ -3,14 +3,14 @@ package com.example.worker.core
 import kotlin.reflect.KClass
 
 object Dependencies {
-    val registry: MutableMap<KClass<*>, Any> = mutableMapOf()
+    val registry: MutableMap<KClass<*>, () -> Any> = mutableMapOf()
 
-    fun <T : Any> provide(type: KClass<T>, dependency: T) {
+    fun <T : Any> provide(type: KClass<T>, dependency: () -> T) {
         registry[type] = dependency
     }
 
-    fun <T : Any> resolve(type: KClass<T>) : Any? {
-        return registry[type]
+    fun <T : Any> resolve(type: KClass<T>) : T? {
+        return registry[type]?.invoke() as T?
     }
 }
 
