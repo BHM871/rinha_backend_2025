fe98966d543d21f5f452b203f3eb0d30142464e0
Adrian Almeida <adrianalmeida417@gmail.com>
2025-07-28 18:04:27 -0300

    chore: implementing enqueue in Worker

--- src/worker/src/main/kotlin/com/example/worker/repository/RedisRepository.kt
+++ src/worker/src/main/kotlin/com/example/worker/repository/RedisRepository.kt
@@ -1,14 +1,23 @@
 package com.example.worker.repository
 
-import com.example.models.core.Payment
 import com.example.redis.core.app.Event
 import com.example.redis.core.app.Mediator
 import com.example.redis.core.app.components.Queuer
 import com.example.redis.core.app.components.Storage
+import java.math.BigDecimal
 
 class RedisRepository(
     override val mediator: Mediator
 ) : Queuer, Storage {
+    override fun enqueue(score: BigDecimal, payment: String): Boolean {
+        return try {
+            this.mediator.notify(this, Event.ENQUEUE, score, payment)
+            true
+        } catch (_: Exception) {
+            false
+        }
+    }
+
     override fun dequeue(reverse: Boolean): String? {
         return this.mediator.notify(this, Event.DEQUEUE, reverse) as String?
     }
