3758a409f578c84fd16fa72f3316db80b3eeb704
Adrian Almeida <adrianalmeida417@gmail.com>
2025-08-04 18:41:10 -0300

    chore: implementing In Memory Mediator

---
+++ src/database/src/main/kotlin/com/example/redis/InMemoryMediator.kt
@@ -0,0 +1,35 @@
+package com.example.redis
+
+import com.example.models.core.FilterSummary
+import com.example.models.core.Payment
+import com.example.redis.core.app.Component
+import com.example.redis.core.app.Event
+import com.example.redis.core.app.Mediator
+import com.example.redis.core.app.components.Queuer
+import com.example.redis.core.app.components.Storage
+import com.example.redis.middleware.InMemoryQueue
+import com.example.redis.middleware.InMemoryStorage
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+class InMemoryMediator : Mediator {
+
+    private val queue = InMemoryQueue()
+    private val storage = InMemoryStorage()
+
+    override fun notify(component: Component, event: Event, vararg data: Any?) : Any? {
+        return when (component) {
+            is Queuer -> when (event) {
+                Event.ENQUEUE -> queue.enqueue(data[0]!! as Payment)
+                Event.DEQUEUE -> queue.dequeue(data[0] as Boolean? ?: false)
+                else -> null
+            }
+            is Storage -> when(event) {
+                Event.STORAGE -> storage.store(data[0]!! as BigDecimal, data[1]!! as LocalDateTime, data[2]!! as Boolean)
+                Event.SUMMARY -> storage.getSummary(data[0]!! as FilterSummary)
+                else -> null
+            }
+            else -> null
+        }
+    }
+}
